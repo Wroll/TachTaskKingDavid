55e99a2a06b6e2af3b579a8ae99367181f1aefa4
dmitriy <40537103+Wroll@users.noreply.github.com>
2020-08-02 16:58:13 +0300

    Update input_data.py

--- input_data.py
+++ input_data.py
@@ -1,6 +1,6 @@
 class InputData:
-    ADMIN_EMAIL = 'paula47@example.com'
-    ADMIN_PASSWORD = '123456'
+    ADMIN_EMAIL = ''
+    ADMIN_PASSWORD = ''
     FIRSTNAME_OWNER = 'firstname'
     LASTNAME_OWNER = 'lastname'
     EMAIL_OWNER = 'randomchipchip@gmail.com'
